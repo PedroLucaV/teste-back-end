d03e921007976bf1f07808c05bf1ea65c13e865f
Pedro Lucas <izielucas2016@gmail.com>
2024-09-02 12:26:09 -0300

    created all tests

---
+++ tests/aula-01/index.js
@@ -0,0 +1,8 @@
+export const areaQuadrado = (comprimento, largura) => {
+    return comprimento * largura;   
+}
+
+export const listaFruta = () => {
+    const frutas = ["Pera", "Maçã", "Uva", "Ameixa", "Umbu"]
+    return frutas
+}
--- tests/aula-01/index.test.js
+++ tests/aula-01/index.test.js
@@ -1,7 +1,15 @@
-test("Verifica area do quadrado", () => {
-    const areaQuadrado = (comprimento, largura) => {
-        return comprimento * largura
-    }
+import { areaQuadrado, listaFruta } from "./index.js";
 
+test("Verifica area do quadrado", () => {
     expect(areaQuadrado(5, 5)).toBe(25);
+})
+
+test("Compara String", () => {
+    expect("Pedro").toBe("Pedro")
+    expect("Pedro").not.toBe("lucas")
+    expect("Pedro").toEqual("Pedro") //analisa mais profundo (Objeto, Array e etc)
+})
+
+test("Verifica se tem Umbu", () => {
+    expect(listaFruta()).toContain("Umbu")
 })
